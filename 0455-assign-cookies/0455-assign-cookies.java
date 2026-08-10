94d111f60652b0be989b85516acfcc5f38a9727b
Krish Baansal <krishbaansal@gmail.com>
2026-08-09 16:35:40 -0800

    Time: 15 ms (59.8%), Space: 51.7 MB (19.53%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int i=0;
+        int j=0;
+        int res=0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        while(i<g.length && j<s.length){
+            if(s[j]>=g[i]){
+                res++;
+                i++;
+                j++;
+            }
+            else if(s[j]<g[i]){
+                j++;
+            }
+        }
+        return res;
+    }
+}
