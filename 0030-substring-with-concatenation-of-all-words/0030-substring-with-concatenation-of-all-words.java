1c7bd1967e65c3225cadfdf587025a1e8ae970c5
Krish Baansal <krishbaansal@gmail.com>
2026-09-04 13:09:25 +0530

    Time: 11 ms (96.36%), Space: 47.4 MB (24.57%) - LeetHub

---
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
@@ -0,0 +1,86 @@
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+
+        List<Integer> result = new ArrayList<>();
+
+        if (s.length() == 0 || words.length == 0) {
+            return result;
+        }
+
+        int wordLen = words[0].length();
+        int wordCount = words.length;
+        int totalLen = wordLen * wordCount;
+
+        if (s.length() < totalLen) {
+            return result;
+        }
+
+        
+        Map<String, Integer> need = new HashMap<>();
+
+        for (String word : words) {
+            need.put(word, need.getOrDefault(word, 0) + 1);
+        }
+
+        
+        for (int offset = 0; offset < wordLen; offset++) {
+
+            int left = offset;
+            int count = 0;
+
+            Map<String, Integer> window = new HashMap<>();
+
+            for (int right = offset;
+                 right + wordLen <= s.length();
+                 right += wordLen) {
+
+                String word = s.substring(right, right + wordLen);
+
+               
+                if (!need.containsKey(word)) {
+                    window.clear();
+                    count = 0;
+                    left = right + wordLen;
+                    continue;
+                }
+
+                window.put(word, window.getOrDefault(word, 0) + 1);
+                count++;
+
+                
+                while (window.get(word) > need.get(word)) {
+
+                    String leftWord =
+                            s.substring(left, left + wordLen);
+
+                    window.put(
+                        leftWord,
+                        window.get(leftWord) - 1
+                    );
+
+                    left += wordLen;
+                    count--;
+                }
+
+            
+                if (count == wordCount) {
+                    result.add(left);
+
+                    
+                    String leftWord =
+                            s.substring(left, left + wordLen);
+
+                    window.put(
+                        leftWord,
+                        window.get(leftWord) - 1
+                    );
+
+                    left += wordLen;
+                    count--;
+                }
+            }
+        }
+
+        return result;
+    }
+}
